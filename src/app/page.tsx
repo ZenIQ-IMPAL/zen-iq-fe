e3f7c19e9a105cf75209cc1af4f2c83d6f658d4e
kingkinfajarr <kingkinfajar8@gmail.com>
2025-10-11 13:20:27 +0700

    feat(landing-page): change import button with shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,23 @@
-import { Button } from "@/components/button";
-import Link from "./link";
+'use client"';
+
+import { Footer } from "@/components/footer";
+import { Navbar } from "@/components/navbar";
+import { AboutZenIQSection } from "@/components/section/homepage/about-us-section";
+import { ExploreCourseSection } from "@/components/section/homepage/course-section";
+import { HeroSection } from "@/components/section/homepage/hero";
+import { PricingSection } from "@/components/section/homepage/pricing-section";
+import { SuccessStorySection } from "@/components/section/homepage/success-story-section";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center">
-            <h1 className="font-bold text-3xl">Welcome to Homepage</h1>
-            <div className="flex gap-4 mt-4">
-                <Link href={"/login"}>
-                    <Button type="button" variant="outline">
-                        Login
-                    </Button>
-                </Link>
-                <Link href={"/register"}>
-                    <Button type="button" variant="outline">
-                        Register
-                    </Button>
-                </Link>
-            </div>
+        <main className="">
+            <Navbar />
+            <HeroSection />
+            <ExploreCourseSection />
+            <SuccessStorySection />
+            <AboutZenIQSection />
+            <PricingSection />
+            <Footer />
         </main>
     );
 }
